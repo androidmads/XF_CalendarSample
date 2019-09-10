98cdf503679b4262c011d89bd48d380985b3cfcf
Mushtaq <mushtaqat3gb@gmail.com>
2019-09-09 22:06:10 -0700

    Calendar Sample for Xamarin.Forms

---
+++ CalendarSample/CalendarSample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace CalendarSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    //[assembly:XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CalendarSample/CalendarSample/MainPageModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace CalendarSample
+{
+    public class MainPageModel : BindableObject
+    {
+        private DateTime? _date;
+        public DateTime? Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                _date = value;
+                OnPropertyChanged(nameof(Date));
+            }
+        }
+
+        private ObservableCollection<XamForms.Controls.SpecialDate> attendances;
+        public ObservableCollection<XamForms.Controls.SpecialDate> Attendances
+        {
+            get
+            {
+                return attendances;
+            }
+            set
+            {
+                attendances = value;
+                OnPropertyChanged(nameof(Attendances));
+            }
+        }
+
+        public Command DateChosen
+        {
+            get
+            {
+                return new Command((obj) =>
+                {
+                    System.Diagnostics.Debug.WriteLine(obj as DateTime?);
+                });
+            }
+        }
+    }
+}
